b670ad99fa17fda8a029a8d1f282ac29fe3a6348
Niclas Nilsson <niclas.nilsson@protonmail.com>
2021-09-06 19:23:24 +0200

    WIP

---
+++ impls/cljs/src/main.cljs
@@ -0,0 +1,5 @@
+(ns main
+  (:require [step0-repl :as step0]
+            [step1-read-print :as step1]))
+
+(defn -main [& _args])
---
+++ impls/cljs/src/step1_read_print.cljs
@@ -0,0 +1,26 @@
+(ns step1-read-print
+  (:require ["readline" :as readline]))
+
+(def rl
+  (.createInterface readline #js {:input js/process.stdin
+                                  :output js/process.stdout}))
+
+(defn READ [s]
+  s)
+
+(defn EVAL [s]
+  s)
+
+(defn PRINT [s]
+  (println s))
+
+(defn rep [s]
+  (-> s READ EVAL PRINT))
+
+(defn main-loop []
+  (.question rl "user> " (fn [s]
+                          (rep s)
+                          (main-loop))))
+
+(defn -main [& _args]
+  (main-loop))

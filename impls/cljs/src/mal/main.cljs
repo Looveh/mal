050a214bb52e7ec3e1009b011bbb02df610a2131
Niclas Nilsson <niclas.nilsson@protonmail.com>
2021-09-06 23:06:03 +0200

    Eval ish

--- impls/cljs/src/mal/main.cljs
+++ impls/cljs/src/mal/main.cljs
@@ -1,5 +1,6 @@
 (ns mal.main
   (:require [mal.step0-repl :as step0]
-            [mal.step1-read-print :as step1]))
+            [mal.step1-read-print :as step1]
+            [mal.step2-eval]))
 
 (defn -main [& _args])
---
+++ impls/cljs/src/mal/step2_eval.cljs
@@ -0,0 +1,73 @@
+(ns mal.step2-eval
+  (:require [mal.reader :as reader :refer [->form]]
+            ["readline" :as readline]))
+
+(def rl
+  (.createInterface readline #js {:input js/process.stdin
+                                  :output js/process.stdout}))
+
+(def repl-env
+  {"+" (fn [a b]
+         (->form "number" (+ a b)))
+   "-" (fn [a b]
+         (->form "number" (- a b)))
+   "*" (fn [a b]
+         (->form "number" (* a b)))
+   "/" (fn [a b]
+         (->form "number" (/ a b)))})
+
+(declare eval-ast)
+
+(defn READ [s]
+  (reader/read-str' s))
+
+(defn EVAL [ast env]
+  (cond
+    (not= "list" ast.type)
+    (eval-ast ast env)
+
+    (empty? ast.value)
+    ast
+
+    :else
+    (let [res (eval-ast ast env)
+          _ (println res)
+          operator (aget res.value 0)
+          operands (.slice res.value 1)]
+      (apply operator operands))))
+
+(defn eval-ast [ast env]
+  (let [type ast.type
+        value ast.value]
+    (cond
+      (= "symbol" type)
+      (if-let [symbol (get env value)]
+        symbol
+        (throw (str "Symbol not found: '" value "'")))
+
+      (= "list" type)
+      (reader/->form "list" (let [res #js []]
+                              (doseq [e value]
+                                (.push res (EVAL e env)))
+                              res))
+
+      :else
+      value)))
+
+(defn PRINT [form]
+  (println (reader/pr-str' form)))
+
+(defn rep [s]
+  (-> s
+      (READ)
+      (EVAL repl-env)
+      (PRINT)))
+
+(defn main-loop []
+  (.question rl "user> " (fn [s]
+                           (rep s)
+                           (main-loop))))
+
+(defn -main [& _args]
+  (main-loop))
+
